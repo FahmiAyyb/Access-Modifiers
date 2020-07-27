6edf19a47fcda82c7d4e1df18b652dd0ac12f86c
Muhammad Fahmi Ayyub <50710686+FahmiAyyb@users.noreply.github.com>
2020-07-28 03:14:05 +0800

    Add files via upload

---
+++ AccesModifiers/src/AccessModif/private1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccessModif;
+
+/**
+ *
+ * @author Fahmii
+ */
+public class private1 {
+    private String name;
+
+    public void setName(name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ AccesModifiers/src/AccessModif/public1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccessModif;
+
+/**
+ *
+ * @author Fahmii
+ */
+public class public1 {
+     public String name;
+
+    public public1(String newName){
+        this.name = newName;
+    }
+}
